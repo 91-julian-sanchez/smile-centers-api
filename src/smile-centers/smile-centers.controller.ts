bf83120cd857425afd97927ceb448706fe7c004b
julians.rey <julians.rey@elenas.la>
2024-06-23 11:52:12 -0500

    docs: add Swagger for API documentation

--- src/smile-centers/smile-centers.controller.ts
+++ src/smile-centers/smile-centers.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { SmileCenterService } from './smile-centers.service';
 import { GetSmileCenterDto } from './dto/get-smile-centers.dto';
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
 
+@ApiTags('smile-center')
 @Controller('smile-center')
 export class SmileCenterController {
   constructor(private readonly smileCentersService: SmileCenterService) {}
 
   @Get()
   @UsePipes(new ValidationPipe({ transform: true }))
+  @ApiOperation({ summary: 'Obtener Smile Centers' })
   async getCenters(@Query() getCentersDto: GetSmileCenterDto) {
     return this.smileCentersService.getCenters(getCentersDto);
   }
